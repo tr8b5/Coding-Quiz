b14c0ffeeda4622937f9f93cba4760d1d6bb93ce
William Miller <williamhmiller12@gmail.com>
2020-12-15 21:12:39 -0500

    I added the quiz obj

--- script.js
+++ script.js
@@ -4,28 +4,52 @@ var startQuizEl = document.querySelector("#start");
 var timerEl = document.querySelector("#timer");
 var titleEl = document.querySelector(".title");
 
+var quizObj = {
+  question: ["", "", "", "", ""],
+  answerOne: ["", "", "", "", ""],
+  answerTwo: ["", "", "", "", ""],
+  answerThree: ["", "", "", "", ""],
+  answerFour: ["", "", "", "", ""],
+  correctValue: ["", "", "", "", ""],
+};
+
 descriptionEl.textContent =
   "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
 
 var aEl = document.createElement("button");
 aEl.classList.add("btn");
 aEl.classList.add("btn-primary");
-aEl.setAttribute("style", "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 5%;");
+aEl.setAttribute(
+  "style",
+  "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 5%;"
+);
 
 var bEl = document.createElement("button");
 bEl.classList.add("btn");
 bEl.classList.add("btn-primary");
-bEl.setAttribute("style", "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 1%;");
+bEl.setAttribute(
+  "style",
+  "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 1%;"
+);
 
 var cEl = document.createElement("button");
 cEl.classList.add("btn");
 cEl.classList.add("btn-primary");
-cEl.setAttribute("style", "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 1%;");
+cEl.setAttribute(
+  "style",
+  "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 1%;"
+);
 
 var dEl = document.createElement("button");
 dEl.classList.add("btn");
 dEl.classList.add("btn-primary");
-dEl.setAttribute("style", "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 1%;");
+dEl.setAttribute(
+  "style",
+  "background-color: blueviolet; border: none; margin: left; display: flex; margin-top: 1%;"
+);
+
+
+//functions
 
 viewHighscoresEl.addEventListener("click", function () {});
 
@@ -34,10 +58,10 @@ startQuizEl.addEventListener("click", function () {
   titleEl.setAttribute("style", "text-align: left");
   descriptionEl.remove();
   startQuizEl.remove();
-  aEl.textContent = "strings"
-  bEl.textContent = "booleans"
-  cEl.textContent = "alerts"
-  dEl.textContent = "numbers"
+  aEl.textContent = "strings";
+  bEl.textContent = "booleans";
+  cEl.textContent = "alerts";
+  dEl.textContent = "numbers";
   document.querySelector(".column").appendChild(aEl);
   document.querySelector(".column").appendChild(bEl);
   document.querySelector(".column").appendChild(cEl);
